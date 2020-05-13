3cb06ea84ce0b6eee532b76df5f9eba624db6a07
Michael Burbea <michaelburbea@gmail.com>
2020-05-12 20:03:07 -0400

    modern pdb?

--- ByteManager/Properties/AssemblyInfo.cs
+++ ByteManager/Properties/AssemblyInfo.cs
@@ -1,12 +1,5 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Resources;
-
-[assembly: AssemblyTitle("KoAR Item Editor")]
-[assembly: AssemblyDescription("Allows editing of items in KoAR saves.")]
-[assembly: AssemblyCopyright("Copyright © 2019 Jerome Montino")]
 [assembly: ComVisible(false)]
 [assembly: Guid("b009dd9c-d3ac-4e64-bc4f-65c55b011391")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: NeutralResourcesLanguageAttribute("en-US")]
