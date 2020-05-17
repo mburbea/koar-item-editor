81058a8873bda94f5666b29e7775ecb26685048d
Michael Burbea <michaelburbea@gmail.com>
2020-05-17 15:55:26 -0400

    Remove unused usings.

--- ItemTesting/Program.cs
+++ ItemTesting/Program.cs
@@ -2,9 +2,7 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
-using System.Runtime.InteropServices;
 using System.Linq;
-using System.Text;
 using System.Diagnostics;
 
 namespace ItemTesting
--- KoAR.SaveEditor/Views/ItemModel.cs
+++ KoAR.SaveEditor/Views/ItemModel.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Windows.Threading;
 using KoAR.Core;
 using KoAR.SaveEditor.Constructs;
 
