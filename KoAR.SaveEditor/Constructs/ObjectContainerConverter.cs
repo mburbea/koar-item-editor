16472d31843c9d81da2e4161d0f417bdfb00bcb9
Amir Burbea <amir.burbea@gmail.com>
2020-05-19 10:32:31 -0500

    Equipment type filter enhancement

---
+++ KoAR.SaveEditor/Constructs/ObjectContainerConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+
+namespace KoAR.SaveEditor.Constructs
+{
+    public sealed class ObjectContainerConverter : IValueConverter
+    {
+        object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is IEnumerable collection
+                ? collection.Cast<object>().Select(item => new ObjectContainer(item)).ToArray()
+                : (object)new ObjectContainer(value);
+        }
+
+        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
