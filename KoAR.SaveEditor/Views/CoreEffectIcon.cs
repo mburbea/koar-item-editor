5b746a4771dff7fa1da7a23a2ad9fe74c3c5f3e6
Amir Burbea <amir.burbea@gmail.com>
2020-05-21 12:33:30 -0500

    Rename icon classes

---
+++ KoAR.SaveEditor/Views/CoreEffectIcon.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using KoAR.Core;
+
+namespace KoAR.SaveEditor.Views
+{
+    public sealed class CoreEffectIcon : Control
+    {
+        public static readonly DependencyProperty CoreEffectProperty = DependencyProperty.Register(nameof(CoreEffectIcon.CoreEffect), typeof(CoreEffectInfo), typeof(CoreEffectIcon),
+            new PropertyMetadata());
+
+        static CoreEffectIcon()
+        {
+            FrameworkElement.DefaultStyleKeyProperty.OverrideMetadata(typeof(CoreEffectIcon), new FrameworkPropertyMetadata(typeof(CoreEffectIcon)));
+        }
+
+        public CoreEffectInfo? CoreEffect
+        {
+            get => (CoreEffectInfo?)this.GetValue(CoreEffectIcon.CoreEffectProperty);
+            set => this.SetValue(CoreEffectIcon.CoreEffectProperty, value);
+        }
+    }
+}
