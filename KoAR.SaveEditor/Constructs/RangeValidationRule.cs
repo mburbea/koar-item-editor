6972dfcd464051e458154a04c03f8dc0feff3c6c
Michael Burbea <michaelburbea@gmail.com>
2020-05-27 22:25:41 -0400

    Make america greater again.

--- KoAR.SaveEditor/Constructs/RangeValidationRule.cs
+++ KoAR.SaveEditor/Constructs/RangeValidationRule.cs
@@ -5,21 +5,19 @@ namespace KoAR.SaveEditor.Constructs
 {
     public sealed class RangeValidationRule : ValidationRule
     {
-        public long Min { get; set; } = long.MinValue;
-
         public long Max { get; set; } = long.MaxValue;
+        
+        public long Min { get; set; } = long.MinValue;
 
         public NumberStyles NumberStyle { get; set; } = NumberStyles.Any;
 
         public override ValidationResult Validate(object value, CultureInfo cultureInfo)
         {
             return value is string text
-                && text.Length != 0
-                && long.TryParse(text, NumberStyle, cultureInfo, out long number)
-                && (number >= Min)
-                && (number <= Max)
+                && long.TryParse(text, this.NumberStyle, cultureInfo, out long number)
+                && number >= this.Min && number <= this.Max
                 ? ValidationResult.ValidResult
-                : new ValidationResult(false, $"Value must be a number between {Min} and {Max}.");
+                : new ValidationResult(false, $"Value must be a number between {this.Min} and {this.Max}.");
         }
     }
 }
