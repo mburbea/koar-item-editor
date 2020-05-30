a95d07df815d3638869cc26f9e794b2bfa518793
Michael Burbea <michaelburbea@gmail.com>
2020-05-30 00:01:14 -0400

    Remove uneeded using

--- KoAR.Core/NotNullWhenAttribute.cs
+++ KoAR.Core/NotNullWhenAttribute.cs
@@ -1,9 +1,7 @@
-﻿using System;
-
-namespace System.Diagnostics.CodeAnalysis
+﻿namespace System.Diagnostics.CodeAnalysis
 {
     // not including in .net framework for whatever reason.
-    internal class NotNullWhenAttribute : Attribute
+    internal sealed class NotNullWhenAttribute : Attribute
     {
         public NotNullWhenAttribute(bool _)
         { 
