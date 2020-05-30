a720db7b5c72292d9b1616ae24e906eecb99dc3c
Amir Burbea <amir.burbea@gmail.com>
2020-05-29 20:08:18 -0500

    Cleanup

--- KoAR.Core/Amalur.cs
+++ KoAR.Core/Amalur.cs
@@ -6,7 +6,6 @@ using System.IO;
 using System.Linq;
 using System.Xml.Linq;
 
-
 namespace KoAR.Core
 {
     /// <summary>
--- KoAR.Core/ItemMemoryInfo.Offset.cs
+++ KoAR.Core/ItemMemoryInfo.Offset.cs
@@ -1,18 +1,19 @@
 ﻿namespace KoAR.Core
 {
-    public partial class ItemMemoryInfo
+    partial class ItemMemoryInfo
     {
-        public readonly struct Offset
+        private readonly struct Offset
         {
             public const int EffectCount = 21;
             public const int FirstEffect = EffectCount + 4;
 
             private readonly int _count;
+
             public Offset(int count) => _count = count;
+
             public int PostEffect => FirstEffect + _count * 8;
             public int CurrentDurability => PostEffect + 4;
             public int MaxDurability => CurrentDurability + 4;
-
             public int SellableFlag => MaxDurability + 8;
             public int HasCustomName => SellableFlag + 2;
             public int CustomNameLength => HasCustomName + 1;
