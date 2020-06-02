da607f4025a7d8a09607f0fa7b6800395b47d3bc
Amir Burbea <amir.burbea@gmail.com>
2020-06-01 19:54:33 -0500

    Condensed

--- KoAR.SaveEditor/Views/TitleCaseWordsConverter.cs
+++ KoAR.SaveEditor/Views/TitleCaseWordsConverter.cs
@@ -1,15 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Data;
 
 namespace KoAR.SaveEditor.Views
 {
-    public class TitleCaseWordsConverter : IValueConverter
+    public sealed class TitleCaseWordsConverter : IValueConverter
     {
         object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
@@ -18,24 +16,14 @@ namespace KoAR.SaveEditor.Views
                 return DependencyProperty.UnsetValue;
             }
             string word = value.ToString();
-            int space = -1;
-            for (int i = 1; i < word.Length; i++)
+            return word.Skip(1).Aggregate(new StringBuilder(word.Length + 1).Append(word[0]), (builder, c) =>
             {
-                if (char.IsUpper(word, i))
+                if (Char.IsUpper(c))
                 {
-                    space = i;
-                    break;
+                    builder.Append(' ');
                 }
-            }
-            if (space == -1)
-            {
-                return word;
-            }
-            char[] buffer = new char[word.Length + 1];
-            word.AsSpan(0, space).CopyTo(buffer);
-            buffer[space] = ' ';
-            word.AsSpan(space).CopyTo(buffer.AsSpan(space + 1));
-            return new string(buffer);
+                return builder.Append(c);
+            }).ToString();
         }
 
         object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
