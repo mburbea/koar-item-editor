49d23e682920e97f7f4d532e4729c3282896c439
Michael Burbea <michaelburbea@gmail.com>
2020-06-01 21:18:46 -0400

    One liner of dumbness.

--- KoAR.SaveEditor/Views/TitleCaseWordsConverter.cs
+++ KoAR.SaveEditor/Views/TitleCaseWordsConverter.cs
@@ -7,22 +7,14 @@ namespace KoAR.SaveEditor.Views
 {
     public sealed class TitleCaseWordsConverter : IValueConverter
     {
+        static readonly char[] AllCaps = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
         object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (value is null)
             {
                 return DependencyProperty.UnsetValue;
             }
-            string text = value.ToString();
-            int index = 1;
-            for (; index < text.Length; index++)
-            {
-                if (char.IsUpper(text, index))
-                {
-                    break;
-                }
-            }
-            return index==text.Length ? text : $"{text.Substring(0, index)} {text.Substring(index)}";
+            return value.ToString() is string text && text.AsSpan(1).IndexOfAny(AllCaps) is int index && index != -1 ? $"{text.Substring(0, index + 1)} {text.Substring(index + 1)}" : value.ToString();
         }
 
         object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
