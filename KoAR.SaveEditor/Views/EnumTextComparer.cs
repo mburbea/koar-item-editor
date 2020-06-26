66724c03d3a20baae3382856216f833329bf16bc
Amir Burbea <amir.burbea@gmail.com>
2020-06-25 22:41:18 -0500

    Add EnumTextConverter

---
+++ KoAR.SaveEditor/Views/EnumTextComparer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using KoAR.SaveEditor.Constructs;
+
+namespace KoAR.SaveEditor.Views
+{
+    public sealed class EnumTextComparer : IComparer
+    {
+        public static readonly EnumTextComparer Instance = new EnumTextComparer();
+
+        private EnumTextComparer()
+        {
+        }
+
+        public int Compare(object? x, object? y)
+        {
+            if (x == y)
+            {
+                return 0;
+            }
+            if (y is null)
+            {
+                return 1;
+            }
+            return x switch
+            {
+                null => -1,
+                DataContainer left when y is DataContainer right => this.Compare(left.Data, right.Data),
+                Enum left when y is Enum right => string.Compare(left.ToString(), right.ToString()),
+                _ => throw new ArgumentException(!(x is Enum) ? nameof(x) : nameof(y))
+            };
+        }
+    }
+}
