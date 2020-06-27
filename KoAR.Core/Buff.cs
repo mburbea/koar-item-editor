a2411a2e48db61f7c1eaa607217d244dbceff373
Michael Burbea <michaelburbea@gmail.com>
2020-06-27 18:26:00 -0400

    Check in raw file. add a json lib

---
+++ KoAR.Core/Buff.cs
@@ -0,0 +1,21 @@
+﻿namespace KoAR.Core
+{
+    public class Buff
+    {
+        public uint Id { get; set; }
+        public string? Modifier { get; set; }
+        public string? InternalName { get; set; }
+        public Rarity Rarity { get; set; }
+        public string? Ap { get; set; }
+        public BuffDescription[]? BuffDescription { get; set; }
+    }
+
+    public class BuffDescription
+    {
+        public string? ParamIcon { get; set; }
+        public string? Text { get; set; }
+        public string? BuffId { get; set; }
+        public object[]? ParamDescArgs { get; set; }
+        public string? ParamDesc { get; set; }
+    }
+}
