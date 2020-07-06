002198a8ecaeaa6a44e289772f26904c84aebf8a
Michael Burbea <michaelburbea@gmail.com>
2020-07-06 13:29:32 -0400

    Use Nullable Package.

--- KoAR.Core/System.Diagnostics.CodeAnalysis/NotNullWhenAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace System.Diagnostics.CodeAnalysis
-{
-    public sealed class NotNullWhenAttribute : Attribute
-    {
-        public NotNullWhenAttribute(bool _)
-        { 
-        }
-    }
-}
