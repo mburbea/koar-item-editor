d98ba61495603b99ba5dda6a0770ed5ef7ae8a6c
Michael Burbea <michaelburbea@gmail.com>
2020-07-07 19:14:41 -0400

    Created and use Icon controls

---
+++ KoAR.SaveEditor/Views/ArmorTypeIcon.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using KoAR.Core;
+
+namespace KoAR.SaveEditor.Views
+{
+    public class ArmorTypeIcon : Control
+    {
+        public static readonly DependencyProperty ArmorTypeProperty = DependencyProperty.Register(nameof(ArmorTypeIcon.ArmorType), typeof(ArmorType), typeof(ArmorTypeIcon),
+    new PropertyMetadata(ArmorType.None));
+
+        static ArmorTypeIcon() => FrameworkElement.DefaultStyleKeyProperty.OverrideMetadata(typeof(ArmorTypeIcon), new FrameworkPropertyMetadata(typeof(ArmorTypeIcon)));
+
+        public ArmorType ArmorType
+        {
+            get => (ArmorType)this.GetValue(ArmorTypeIcon.ArmorTypeProperty);
+            set => this.SetValue(ArmorTypeIcon.ArmorTypeProperty, value);
+        }
+    }
+}
