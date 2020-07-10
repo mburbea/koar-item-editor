85f51b66bd217644ccc4c149cfc07bb6946a4beb
Amir Burbea <amir.burbea@gmail.com>
2020-07-10 17:04:13 -0500

    Minor cleanup

--- KoAR.SaveEditor/Views/ScrollingTabControl.cs
+++ KoAR.SaveEditor/Views/ScrollingTabControl.cs
@@ -206,13 +206,14 @@ namespace KoAR.SaveEditor.Views
             {
                 return;
             }
+            int index = this.SelectedIndex;
             if ((Keyboard.Modifiers & ModifierKeys.Shift) == ModifierKeys.Shift)
             {
-                this.SelectedIndex = (this.SelectedIndex == 0 ? this.Items.Count : this.SelectedIndex) - 1;
+                this.SelectedIndex = --index < 0 ? this.Items.Count - 1 : index;
             }
             else
             {
-                this.SelectedIndex = (this.SelectedIndex == this.Items.Count - 1 ? 0 : (this.SelectedIndex + 1));
+                this.SelectedIndex = ++index < this.Items.Count ? index : 0;
             }
         }
     }
