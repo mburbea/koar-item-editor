b3862a9dcc71881f4939e71973287f18ecabeef7
Michael Burbea <michaelburbea@gmail.com>
2020-07-26 16:15:00 -0400

    Remove useless stuff.

--- KoAR.Core/GemSocket.cs
+++ KoAR.Core/GemSocket.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Net.Sockets;
-using System.Text;
-
-namespace KoAR.Core
+﻿namespace KoAR.Core
 {
     public readonly struct GemSocket
     {
