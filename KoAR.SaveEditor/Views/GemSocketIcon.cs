9517cb07844462aa961176824816ad884b90e3ec
Amir Burbea <amir.burbea@gmail.com>
2020-07-26 15:46:11 -0500

    Add stub of gemsocketicon

---
+++ KoAR.SaveEditor/Views/GemSocketIcon.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using KoAR.Core;
+
+namespace KoAR.SaveEditor.Views
+{
+    public sealed class GemSocketIcon : Control
+    {
+        public static readonly DependencyProperty GemSocketProperty = DependencyProperty.Register(nameof(GemSocketIcon.GemSocket), typeof(GemSocket), typeof(GemSocketIcon));
+
+        static GemSocketIcon() => FrameworkElement.DefaultStyleKeyProperty.OverrideMetadata(typeof(GemSocketIcon), new FrameworkPropertyMetadata(typeof(GemSocketIcon));
+
+        public GemSocket GemSocket
+        {
+            get => (GemSocket)this.GetValue(GemSocketIcon.GemSocketProperty);
+            set => this.SetValue(GemSocketIcon.GemSocketProperty, value);
+        }
+    }
+}
