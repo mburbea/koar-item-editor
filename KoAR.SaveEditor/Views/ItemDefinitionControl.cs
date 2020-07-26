af1f6af7390211727fc3ac408db315ebeb8a1cd6
Michael Burbea <michaelburbea@gmail.com>
2020-07-26 17:38:59 -0400

    Started ItemDef control

--- KoAR.SaveEditor/Views/ItemDefinitionControl.cs
+++ KoAR.SaveEditor/Views/ItemDefinitionControl.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Globalization;
 using System.Windows;
@@ -10,14 +12,22 @@ namespace KoAR.SaveEditor.Views
 {
     public sealed class ItemDefinitionControl : Control
     {
-        public static readonly DependencyProperty DefinitionProperty = DependencyProperty.Register(nameof(ItemDefinitionControl.Definition), typeof(ItemDefinition), typeof(ItemDefinitionControl));
+        private static readonly DependencyPropertyKey GemSocketsPropertyKey = DependencyProperty.RegisterReadOnly(nameof(GemSockets), typeof(IEnumerable<GemSocket>), typeof(ItemDefinitionControl), new PropertyMetadata());
+        public static readonly DependencyProperty DefinitionProperty = DependencyProperty.Register(nameof(ItemDefinitionControl.Definition), typeof(ItemDefinition), typeof(ItemDefinitionControl),
+            new PropertyMetadata(ItemDefinitionControl.Definition_ValueChanged));
+
+        public static readonly DependencyProperty GemSocketsProperty;
 
         public static readonly DependencyProperty ItemProperty = DependencyProperty.Register(nameof(ItemDefinitionControl.Item), typeof(ItemModelBase), typeof(ItemDefinitionControl),
             new PropertyMetadata(ItemDefinitionControl.ItemProperty_ValueChanged));
 
         public static readonly IValueConverter SocketTextConverter = new SocketLabelConverter();
 
-        static ItemDefinitionControl() => FrameworkElement.DefaultStyleKeyProperty.OverrideMetadata(typeof(ItemDefinitionControl), new FrameworkPropertyMetadata(typeof(ItemDefinitionControl)));
+        static ItemDefinitionControl()
+        {
+            GemSocketsProperty = GemSocketsPropertyKey.DependencyProperty;
+            FrameworkElement.DefaultStyleKeyProperty.OverrideMetadata(typeof(ItemDefinitionControl), new FrameworkPropertyMetadata(typeof(ItemDefinitionControl)));
+        }
 
         public ItemDefinition? Definition
         {
@@ -30,6 +40,24 @@ namespace KoAR.SaveEditor.Views
             get => (ItemModelBase?)this.GetValue(ItemDefinitionControl.ItemProperty);
             set => this.SetValue(ItemDefinitionControl.ItemProperty, value);
         }
+
+        public IEnumerable<GemSocket>? GemSockets
+        {
+            get => (IEnumerable<GemSocket>?)this.GetValue(ItemDefinitionControl.GemSocketsProperty);
+            private set => this.SetValue(ItemDefinitionControl.GemSocketsPropertyKey, value);
+        }
+
+
+        private static void Definition_ValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ItemDefinitionControl control = (ItemDefinitionControl)d;
+            if (control.Item != null)
+            {
+                return;
+            }
+            control.GemSockets = ((ItemDefinition?)e.NewValue)?.GetGemSockets();
+        }
+
 
         private static void ItemProperty_ValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
@@ -44,10 +72,18 @@ namespace KoAR.SaveEditor.Views
             {
                 control.Definition = item.Definition;
                 PropertyChangedEventManager.AddHandler(item, control.Item_DefinitionChanged, nameof(item.Definition));
+                control.GemSockets = item.Item.GetGemSockets();
+            }
+            else
+            {
+                control.GemSockets = null;
             }
         }
 
-        private void Item_DefinitionChanged(object sender, EventArgs e) => this.Definition = ((ItemModelBase)sender).Definition;
+        private void Item_DefinitionChanged(object sender, EventArgs e)
+        {
+            this.Definition = ((ItemModelBase)sender).Definition;
+        }
 
         private sealed class SocketLabelConverter : IValueConverter
         {
