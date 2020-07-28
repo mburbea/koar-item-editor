d443ab57bc98f7fa351896d368ce30f09d1843d5
Amir Burbea <amir.burbea@gmail.com>
2020-07-27 20:56:57 -0500

    Use single tree for BuffCollectionManager (#35)
    * Work in progress
    
    * Buff display
    
    * Fix Whitespace

---
+++ KoAR.SaveEditor/Views/BuffDescriptionsConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using KoAR.Core;
+
+namespace KoAR.SaveEditor.Views
+{
+    public sealed class BuffDescriptionsConverter : IValueConverter
+    {
+        private static readonly BuffDescription[] _empty = { BuffDescription.Empty };
+
+        object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is Buff buff))
+            {
+                return DependencyProperty.UnsetValue;
+            }
+            return buff.Descriptions.Length != 0 ? buff.Descriptions : BuffDescriptionsConverter._empty;
+        }
+
+        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
+}
