58885b48c67194e15e821e5746c679f3254d8fe9
mburbea <michaelburbea@gmail.com>
2020-07-30 22:48:26 -0400

    Add logic to decipher questItems.

--- KoAR.Core/InventoryState.cs
+++ KoAR.Core/InventoryState.cs
@@ -6,6 +6,6 @@
         FromStolenSource = 1 << 2, // Doesn't seem to be checked. 
         Stolen = 1 << 3,
         Unstashable = 1 << 6,
-        Unsellable = 1 << 7,
+        Unsellable = 1 << 7
     }
 }
---
+++ KoAR.Core/QuestItemDefinition.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace KoAR.Core
+{
+    public class QuestItemDefinition
+    {
+        public uint Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        [JsonPropertyName("internal_name")]
+        public string InternalName { get; set; } = string.Empty;
+    }
+}
