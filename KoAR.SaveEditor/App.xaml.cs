625ddb5cd51b7bf09b403e62a7bec768e12199ae
Amir Burbea <amir.burbea@gmail.com>
2020-07-31 09:44:04 -0500

    Cleanup

--- KoAR.SaveEditor/App.xaml.cs
+++ KoAR.SaveEditor/App.xaml.cs
@@ -1,14 +1,10 @@
 ﻿using System;
-using KoAR.Core;
 using Microsoft.Windows.Themes;
 
 namespace KoAR.SaveEditor
 {
     partial class App
     {
-        static App()
-        {
-            Type.GetTypeCode(typeof(PlatformCulture)); // Needed to enforce loading of PresentationFramework.Aero.dll before initializing App.
-        }
+        static App() => Type.GetTypeCode(typeof(PlatformCulture)); // Needed to enforce loading of PresentationFramework.Aero.dll before initializing App.
     }
 }
