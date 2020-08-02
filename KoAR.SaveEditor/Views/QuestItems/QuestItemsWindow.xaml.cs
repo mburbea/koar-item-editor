186366a99a4134d0994cba4233d80b5b361b89ab
Amir Burbea <amir.burbea@gmail.com>
2020-08-02 11:23:42 -0500

    Remove imports

--- KoAR.SaveEditor/Views/QuestItems/QuestItemsWindow.xaml.cs
+++ KoAR.SaveEditor/Views/QuestItems/QuestItemsWindow.xaml.cs
@@ -1,27 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-
-namespace KoAR.SaveEditor.Views.QuestItems
+﻿namespace KoAR.SaveEditor.Views.QuestItems
 {
-    /// <summary>
-    /// Interaction logic for QuestItemsWindow.xaml
-    /// </summary>
-    public partial class QuestItemsWindow : Window
+    partial class QuestItemsWindow
     {
-        public QuestItemsWindow()
-        {
-            InitializeComponent();
-        }
+        public QuestItemsWindow() => this.InitializeComponent();
     }
 }
