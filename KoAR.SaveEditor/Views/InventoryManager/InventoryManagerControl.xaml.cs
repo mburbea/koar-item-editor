8fab95d6bac16e25346df651789d7a93d6a3c0d1
Amir Burbea <amir.burbea@gmail.com>
2020-08-03 12:57:44 -0500

    Compress data files (#45)
    * Use data as zip file
    
    * rename variable
    
    * Make static variable public
    
    * Uncompress zip file and replace with 0 byte
    
    * Update gitignore and build zip file
    
    * Boom
    
    * This is dumb.
    
    Co-authored-by: mburbea <michaelburbea@gmail.com>

--- KoAR.SaveEditor/Views/InventoryManager/InventoryManagerControl.xaml.cs
+++ KoAR.SaveEditor/Views/InventoryManager/InventoryManagerControl.xaml.cs
@@ -1,28 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace KoAR.SaveEditor.Views.InventoryManager
+﻿namespace KoAR.SaveEditor.Views.InventoryManager
 {
-    /// <summary>
-    /// Interaction logic for InventoryManagerControl.xaml
-    /// </summary>
-    public partial class InventoryManagerControl : UserControl
+    partial class InventoryManagerControl
     {
-        public InventoryManagerControl()
-        {
-            InitializeComponent();
-        }
+        public InventoryManagerControl() => this.InitializeComponent();
     }
 }
