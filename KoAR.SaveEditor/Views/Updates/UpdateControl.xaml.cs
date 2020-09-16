c3e53a7d0c74c26e9a3f8dc8be2bbdc186e477b7
Amir Burbea <amir.burbea@gmail.com>
2020-09-15 19:53:24 -0500

    Simplify for interim release

---
+++ KoAR.SaveEditor/Views/Updates/UpdateControl.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace KoAR.SaveEditor.Views.Updates
+{
+    partial class UpdateControl
+    {
+        private Window? _window;
+
+        public UpdateControl()
+        {
+            this.InitializeComponent();
+            this.Loaded += this.UpdateControl_Loaded;
+        }
+
+        protected override void OnInitialized(EventArgs e)
+        {
+            base.OnInitialized(e);
+        }
+
+        protected override void OnPropertyChanged(DependencyPropertyChangedEventArgs e)
+        {
+            if (e.Property == FrameworkElement.DataContextProperty && e.NewValue is UpdateViewModelBase viewModel)
+            {
+                PropertyChangedEventManager.AddHandler(viewModel, this.ViewModel_DialogResultChanged, nameof(viewModel.DialogResult));
+            }
+            base.OnPropertyChanged(e);
+        }
+
+        private void RichTextBox_Loaded(object sender, RoutedEventArgs e) => ((RichTextBox)sender).Document = ((UpdateViewModelBase)this.DataContext).Document;
+
+        private void UpdateControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.Loaded -= this.UpdateControl_Loaded;
+            (this._window = Window.GetWindow(this)).Closing += this.Window_Closing;
+        }
+
+        private void ViewModel_DialogResultChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (this._window == null)
+            {
+                return;
+            }
+            this._window.DialogResult = ((UpdateViewModelBase)sender).DialogResult;
+            this._window.Close();
+        }
+
+        private void Window_Closing(object sender, CancelEventArgs e)
+        {
+            ((Window)sender).Closing -= Window_Closing;
+            UpdateViewModelBase viewModel = (UpdateViewModelBase)this.DataContext;
+            PropertyChangedEventManager.RemoveHandler(viewModel, this.ViewModel_DialogResultChanged, nameof(viewModel.DialogResult));
+        }
+    }
+}
--- KoAR.SaveEditor/Views/Updates/UpdateWindow.xaml.cs
+++ KoAR.SaveEditor/Views/Updates/UpdateWindow.xaml.cs
@@ -1,35 +1,7 @@
-﻿using System.ComponentModel;
-using System.Windows;
-using System.Windows.Controls;
-
-namespace KoAR.SaveEditor.Views.Updates
+﻿namespace KoAR.SaveEditor.Views.Updates
 {
     partial class UpdateWindow
     {
         public UpdateWindow() => this.InitializeComponent();
-
-        protected override void OnClosing(CancelEventArgs e)
-        {
-            UpdateViewModelBase viewModel = (UpdateViewModelBase)this.DataContext;
-            PropertyChangedEventManager.RemoveHandler(viewModel, this.ViewModel_DialogResultChanged, nameof(viewModel.DialogResult));
-            base.OnClosing(e);
-        }
-
-        protected override void OnPropertyChanged(DependencyPropertyChangedEventArgs e)
-        {
-            if (e.Property == FrameworkElement.DataContextProperty && e.NewValue is UpdateViewModelBase viewModel)
-            {
-                PropertyChangedEventManager.AddHandler(viewModel, this.ViewModel_DialogResultChanged, nameof(viewModel.DialogResult));
-            }
-            base.OnPropertyChanged(e);
-        }
-
-        private void RichTextBox_Loaded(object sender, RoutedEventArgs e) => ((RichTextBox)sender).Document = ((UpdateViewModelBase)this.DataContext).Document;
-
-        private void ViewModel_DialogResultChanged(object sender, PropertyChangedEventArgs e)
-        {
-            this.DialogResult = ((UpdateViewModelBase)sender).DialogResult;
-            this.Close();
-        }
     }
 }
