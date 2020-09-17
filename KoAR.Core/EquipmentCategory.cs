0367ae6df2ff4070a1440867bf4edb458b5c18eb
michaelburbea <michaelburbea@gmail.com>
2020-09-17 19:12:44 -0400

    Move to diff file.

--- KoAR.Core/EquipmentCategory.cs
+++ KoAR.Core/EquipmentCategory.cs
@@ -1,6 +1,5 @@
 ﻿namespace KoAR.Core
 {
-    using static EquipmentCategory;
     public enum EquipmentCategory
     {
         Hat = 1,
@@ -21,32 +20,5 @@
         Staff,
         Necklace,
         Ring
-    }
-
-    public static class EquipmentCategoryMethods
-    {
-        public static bool IsJewelry(this EquipmentCategory category) 
-            => category == Necklace 
-            || category == Ring;
-
-        public static bool IsWeapon(this EquipmentCategory category) 
-            => category == Chakrams
-            || category == Daggers
-            || category == Faeblades
-            || category == Greatsword
-            || category == Hammer
-            || category == Longbow
-            || category == Longsword
-            || category == Sceptre
-            || category == Staff;
-
-        public static bool IsArmor(this EquipmentCategory category)
-            => category == Hat
-            || category == Torso
-            || category == Robes
-            || category == Legs
-            || category == Hands
-            || category == Feet
-            || category == Shield;
     }
 }
