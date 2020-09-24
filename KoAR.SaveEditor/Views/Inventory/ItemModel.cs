dd5b7d7117a1be0dab17a2d71ab0ce7ac9cb0ae4
Michael Burbea <michaelburbea@gmail.com>
2020-09-23 23:50:57 -0400

    Allow setting affixes for items. (#73)
    * Allow setting affixes for items.
    
    * hopefully better this time.
    
    * retry on getting this data right
    
    * code review

--- KoAR.SaveEditor/Views/Inventory/ItemModel.cs
+++ KoAR.SaveEditor/Views/Inventory/ItemModel.cs
@@ -93,9 +93,15 @@ namespace KoAR.SaveEditor.Views.Inventory
             get => base.Prefix;
             set
             {
-                if (this.SetItemValue(value, $"{nameof(IItem.ItemBuffs)}.{nameof(this.Prefix)}"))
+                if (!this.SetItemValue(value, $"{nameof(IItem.ItemBuffs)}.{nameof(this.Prefix)}"))
                 {
-                    this.OnPropertyChanged(nameof(this.AffixCount));
+                    return;
+                }
+                this.OnPropertyChanged(nameof(this.AffixCount));
+                if (this.Item.Definition.AffixableName)
+                {
+                    this.OnPropertyChanged(nameof(DisplayName));
+                    this.OnPropertyChanged(nameof(DefinitionDisplayName));
                 }
             }
         }
@@ -105,9 +111,15 @@ namespace KoAR.SaveEditor.Views.Inventory
             get => base.Suffix;
             set
             {
-                if (this.SetItemValue(value, $"{nameof(IItem.ItemBuffs)}.{nameof(this.Suffix)}"))
+                if (!this.SetItemValue(value, $"{nameof(IItem.ItemBuffs)}.{nameof(this.Suffix)}"))
                 {
-                    this.OnPropertyChanged(nameof(this.AffixCount));
+                    return;
+                }
+                this.OnPropertyChanged(nameof(this.AffixCount));
+                if (this.Item.Definition.AffixableName)
+                {
+                    this.OnPropertyChanged(nameof(DisplayName));
+                    this.OnPropertyChanged(nameof(DefinitionDisplayName));
                 }
             }
         }
