c2ce20a5d984d7fb9bee4696c8133b300231103d
Michael Burbea <michaelburbea@gmail.com>
2020-09-24 17:40:39 -0400

    Prevent setting affixes on equipped or items as well as or items with strange instance ids (#75)
    * Prevent setting affixes on equipped or items or items with strange instances (shine and shadow).
    
    * Remove using for system.windows

---
+++ KoAR.SaveEditor/Constructs/AndConverter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+
+namespace KoAR.SaveEditor.Constructs
+{
+    public sealed class AndConverter : IMultiValueConverter
+    {
+        object IMultiValueConverter.Convert(object[] values, Type targetType, object parameter, CultureInfo culture) => values.OfType<bool>().All(x => x);
+
+        object[] IMultiValueConverter.ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
+}
