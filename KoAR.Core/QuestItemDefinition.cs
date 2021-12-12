f41d14b2378e5b2e3b6094a60cf9d9502cd0f529
mburbea <michaelburbea@gmail.com>
2021-12-12 00:00:14 -0500

    boom

--- KoAR.Core/QuestItemDefinition.cs
+++ KoAR.Core/QuestItemDefinition.cs
@@ -1,9 +1,8 @@
 ﻿namespace KoAR.Core
 {
-    public sealed class QuestItemDefinition
-    {
-        public uint Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public string InternalName { get; set; } = string.Empty;
-    }
+    public record QuestItemDefinition(
+        uint Id,
+        string Name,
+        string InternalName
+    );
 }
