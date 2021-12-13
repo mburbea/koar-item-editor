d56e749aa38e574eff7bb434428cce7c62a31a92
mburbea <michaelburbea@gmail.com>
2021-12-13 00:02:38 -0500

    Boom.

--- KoAR.Core/GemDefinition.cs
+++ KoAR.Core/GemDefinition.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Threading;
-
-namespace KoAR.Core;
+﻿namespace KoAR.Core;
 
 public record GemDefinition(
     uint TypeId,
