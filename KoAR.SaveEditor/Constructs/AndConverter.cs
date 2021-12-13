cb5db8f8cbb6358e40ff7edc9aab01a089f27841
Amir Burbea <amir.burbea@gmail.com>
2021-12-12 16:10:45 -0800

    Make and/or converters use boolean boxes

--- KoAR.SaveEditor/Constructs/AndConverter.cs
+++ KoAR.SaveEditor/Constructs/AndConverter.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Globalization;
-using System.Linq;
-using System.Windows.Data;
+﻿using System.Linq;
 
 namespace KoAR.SaveEditor.Constructs;
 
-public sealed class AndConverter : IMultiValueConverter
+public sealed class AndConverter : BooleanCombinationConverter
 {
-    object IMultiValueConverter.Convert(object[] values, Type targetType, object parameter, CultureInfo culture) => values.OfType<bool>().All(x => x);
-
-    object[] IMultiValueConverter.ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    public AndConverter() : base(Enumerable.All) { }
 }
