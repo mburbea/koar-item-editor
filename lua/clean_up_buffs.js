c3949243d713370c2436a99981cc35680e544587
mburbea <michaelburbea@gmail.com>
2021-12-15 12:15:56 -0500

    check in clean up js

---
+++ lua/clean_up_buffs.js
@@ -0,0 +1,10 @@
+let format = (...args)=> args[0].replace(/{(\d+)}/g, (_, number)=> typeof args[number] != 'undefined'? args[number] : "");
+
+for(let elem of a){
+    for(let desc of elem.desc){
+        desc.text = format(desc.param_desc,...desc.param_desc_args)
+        delete desc.param_desc;
+        delete desc.param_desc_args;
+    }
+}
+JSON.stringify(a.sort((x,y)=> x.id - y.id))
