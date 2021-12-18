69f4848002c5a3ecd0c00b35efb2ff1bd200b828
mburbea <michaelburbea@gmail.com>
2021-12-17 21:41:39 -0500

    clean up adorner

--- KoAR.SaveEditor/Views/ChaosTierAdorner.cs
+++ KoAR.SaveEditor/Views/ChaosTierAdorner.cs
@@ -1,35 +1,35 @@
-﻿using KoAR.SaveEditor.Constructs;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Media;
 using KoAR.Core;
+using KoAR.SaveEditor.Constructs;
 
-namespace KoAR.SaveEditor.Views
+namespace KoAR.SaveEditor.Views;
+
+internal class ChaosTierAdorner : IndicatorAdornerBase
 {
-    internal class ChaosTierAdorner : IndicatorAdornerBase
+    public static readonly DependencyProperty ChaosTierProperty = DependencyProperty.RegisterAttached(nameof(ItemDefinition.ChaosTier), typeof(char?), typeof(ChaosTierAdorner),
+        new PropertyMetadata(null, ChaosTierAdorner.ChaosTierProperty_ValueChanged));
+
+    private ChaosTierAdorner(FrameworkElement adornedElement, char chaosTier)
+        : base(adornedElement, AdornerPosition.UpperLeft, background: Brushes.CadetBlue, foreground: Brushes.White, chaosTier.ToString()) => this.ToolTip = $"Chaos Tier: {chaosTier}";
+
+    public static char? GetChaosTier(FrameworkElement element) => (char?)element.GetValue(ChaosTierAdorner.ChaosTierProperty);
+
+    public static void SetChaosTier(FrameworkElement element, char? value) => element.SetValue(ChaosTierAdorner.ChaosTierProperty, value);
+
+    private static void ChaosTierProperty_ValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
     {
-        public static readonly DependencyProperty ChaosTierProperty = DependencyProperty.RegisterAttached(nameof(ItemDefinition.ChaosTier), typeof(char?), typeof(ChaosTierAdorner),
-            new PropertyMetadata(null, ChaosTierAdorner.ChaosTierProperty_ValueChanged));
-
-        private ChaosTierAdorner(FrameworkElement adornedElement, char chaosTier)
-            : base(adornedElement, AdornerPosition.UpperLeft, background: Brushes.CadetBlue, foreground: Brushes.White, chaosTier.ToString()) => this.ToolTip = $"Chaos Tier: {chaosTier}";
-        public static char? GetChaosTier(FrameworkElement element) => (char?)element.GetValue(ChaosTierAdorner.ChaosTierProperty);
-
-        public static void SetChaosTier(FrameworkElement element, char? value) => element.SetValue(ChaosTierAdorner.ChaosTierProperty, value);
-
-        private static void ChaosTierProperty_ValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        if (d is not FrameworkElement element)
         {
-            if (d is not FrameworkElement element)
-            {
-                return;
-            }
-            if (e.NewValue is char tier)
-            {
-                IndicatorAdornerBase.SetAdorner(element, new ChaosTierAdorner(element, tier));
-            }
-            else
-            {
-                IndicatorAdornerBase.DetachAdorner<ChaosTierAdorner>(element);
-            }
+            return;
+        }
+        if (e.NewValue is char tier)
+        {
+            IndicatorAdornerBase.SetAdorner(element, new ChaosTierAdorner(element, tier));
+        }
+        else
+        {
+            IndicatorAdornerBase.DetachAdorner<ChaosTierAdorner>(element);
         }
     }
 }
