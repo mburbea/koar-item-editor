855daf530dfd7ab797c56123dc4101445a7e1c3e
Amir Burbea <amir.burbea@gmail.com>
2021-12-26 08:16:57 -0800

    Remove CommandMethods (#132)

--- KoAR.SaveEditor/Constructs/MouseDoubleClick.cs
+++ KoAR.SaveEditor/Constructs/MouseDoubleClick.cs
@@ -16,41 +16,38 @@ public static class MouseDoubleClick
     public static readonly DependencyProperty CommandTargetProperty = DependencyProperty.RegisterAttached("CommandTarget", typeof(IInputElement), typeof(MouseDoubleClick),
         new());
 
-    public static ICommand? GetCommand(Control control)
-    {
-        return (ICommand?)(control ?? throw new ArgumentNullException(nameof(control))).GetValue(MouseDoubleClick.CommandProperty);
-    }
+    public static ICommand? GetCommand(Control control) => (ICommand?)control.GetValue(MouseDoubleClick.CommandProperty);
 
-    public static object? GetCommandParameter(Control control)
-    {
-        return (control ?? throw new ArgumentNullException(nameof(control))).GetValue(MouseDoubleClick.CommandParameterProperty);
-    }
+    public static object? GetCommandParameter(Control control) => control.GetValue(MouseDoubleClick.CommandParameterProperty);
 
-    public static IInputElement? GetCommandTarget(Control control)
-    {
-        return (IInputElement?)(control ?? throw new ArgumentNullException(nameof(control))).GetValue(MouseDoubleClick.CommandTargetProperty);
-    }
+    public static IInputElement? GetCommandTarget(Control control) => (IInputElement?)control.GetValue(MouseDoubleClick.CommandTargetProperty);
 
-    public static void SetCommand(Control control, ICommand? value)
-    {
-        (control ?? throw new ArgumentNullException(nameof(control))).SetValue(MouseDoubleClick.CommandProperty, value);
-    }
+    public static void SetCommand(Control control, ICommand? value) => control.SetValue(MouseDoubleClick.CommandProperty, value);
 
-    public static void SetCommandParameter(Control control, object? value)
-    {
-        (control ?? throw new ArgumentNullException(nameof(control))).SetValue(MouseDoubleClick.CommandParameterProperty, value);
-    }
+    public static void SetCommandParameter(Control control, object? value) => control.SetValue(MouseDoubleClick.CommandParameterProperty, value);
 
-    public static void SetCommandTarget(Control control, IInputElement? value)
-    {
-        (control ?? throw new ArgumentNullException(nameof(control))).SetValue(MouseDoubleClick.CommandTargetProperty, value);
-    }
+    public static void SetCommandTarget(Control control, IInputElement? value) => control.SetValue(MouseDoubleClick.CommandTargetProperty, value);
 
     private static void Control_MouseDoubleClick(object? sender, MouseButtonEventArgs e)
     {
         Control control = (Control)sender!;
-        ICommand? command = MouseDoubleClick.GetCommand(control);
-        command?.TryExecute(MouseDoubleClick.GetCommandParameter(control), MouseDoubleClick.GetCommandTarget(control) ?? control);
+        if (MouseDoubleClick.GetCommand(control) is not { } command)
+        {
+            return;
+        }
+        object? parameter = MouseDoubleClick.GetCommandParameter(control);
+        if (command is RoutedCommand routedCommand)
+        {
+            IInputElement target = MouseDoubleClick.GetCommandTarget(control) ?? control;
+            if (routedCommand.CanExecute(parameter, target))
+            {
+                routedCommand.Execute(parameter, target);
+            }
+        }
+        else if (command.CanExecute(parameter))
+        {
+            command.Execute(parameter);
+        }
     }
 
     private static void OnCommandPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
