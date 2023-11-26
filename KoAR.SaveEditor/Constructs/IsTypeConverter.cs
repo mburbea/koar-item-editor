46792455aa87b9a8a6a5b0e754a893f846b6188b
Amir Burbea <amir.burbea@gmail.com>
2023-11-26 09:27:06 -0800

    More patterns (#164)
    * Further patterns
    
    * use list pattern
    
    * use list pattern
    
    * gg

--- KoAR.SaveEditor/Constructs/IsTypeConverter.cs
+++ KoAR.SaveEditor/Constructs/IsTypeConverter.cs
@@ -2,12 +2,11 @@
 using System.Globalization;
 using System.Windows.Data;
 
-namespace KoAR.SaveEditor.Constructs
-{
-    public sealed class IsTypeConverter : IValueConverter
-    {
-        object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture) => BooleanBoxes.GetBox(parameter is Type type && type.IsInstanceOfType(value));
+namespace KoAR.SaveEditor.Constructs;
 
-        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
-    }
+public sealed class IsTypeConverter : IValueConverter
+{
+    object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture) => BooleanBoxes.GetBox(parameter is Type type && type.IsInstanceOfType(value));
+
+    object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
 }
